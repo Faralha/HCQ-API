ac2ec6d3115afa7a157d62cf316ba907150db693
Zufar <69440085+Faralha@users.noreply.github.com>
2024-02-20 23:46:50 +0700

    Register Handler
    Login di vue masih kurang ni anj

---
+++ db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE 'student' (
+	'id' VARCHAR(10) NOT NULL,
+    'name' VARCHAR(255) NOT NULL,
+	'email' VARCHAR(255) NOT NULL,
+	'password' VARCHAR(255) NOT NULL,
+	'city' VARCHAR(30),
+	'address' VARCHAR(255),
+	'phonenumber' VARCHAR(255) NOT NULL,
+	UNIQUE KEY 'index1' ('id') USING BTREE,
+	PRIMARY KEY ('id')
+);
+
+INSERT INTO 'student' ('id','name','email', 'password', 'city', 'address', 'phonenumber')
+VALUES ('SJKT-0001', 'Muhammad Zufar Al Hafidz','faralhaonly@gmail.com', 'dawdawdas', 'Tangerang Selatan', 'Jl. Walet', '083021730130');
+
+INSERT INTO 'student' ('id','name','email', 'password', 'city', 'address', 'phonenumber')
+VALUES ('SJKT-0002', 'Muhammad Zufar Al Hafidz','faralhaonly@gmail.com', 'dawdawdas', 'Tangerang Selatan', 'Jl. Walet', '083021730130');
+
+INSERT INTO 'student' ('id','name','email', 'password', 'city', 'address', 'phonenumber')
+VALUES (?, ?, ?, ?, ?, ?, ?);
+
+
+SELECT id FROM student ORDER BY id ASC LIMIT 1;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('./db.js');
 require('dotenv').config();
 
 const port = process.env.PORT;
@@ -19,19 +20,79 @@ app.get('/user', (req, res) => {
     });
 })
 
-app.post('/register', (req, res) => {
+// REGISTER (kurang di email kalau udah pernah pakai email yang sama.)
+app.post('/register', async (req, res) => {
 
-    var { email, password, city, address, phonenumber } = req.body;
+    var { email, name, password, city, address, phonenumber } = req.body;
+
+    // BCRYPT PASSWORD HASH
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+
+        // CHECK IF EMAIL HAS BEEN USED
+        const [similar] = await db.execute('SELECT email FROM student WHERE EMAIL = ?', [email]);
+        if (similar.length > 0){
+            return res.status(400).send({ message: 'Email sudah dipakai.' });
+        }
+
+        // SUFFIX NUMBER AUTO INCREMENT
+        const [getId] = await db.execute('SELECT id FROM student ORDER BY id DESC LIMIT 1;');
+        const getIdNumber = getId[0].id.split("-")[1];
+        var lastId = parseInt(getIdNumber, 10);
+
+        lastId++;
+
+        // PREFIX 
+        const role = 'S' // S for Student
+        const city = 'JKT' // JKT for Jakarta as an example
+
+        // NEW PRIMARY KEY
+        let newId = role + city + '-' + lastId.toString().padStart(4, '0');
+
+        await db.execute(`INSERT INTO student ( id, name, email, password, city, address, phonenumber) VALUES (?, ?, ?, ?, ?, ?, ?);`,
+        [newId, name, email, hashedPassword, city, address, phonenumber]);
+
+        res.status(200).json({ message: 'Akun Berhasil Dibuat!'});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error!' });
+    }
 
     console.log(email);
 
-    res.json({
-        message: `User ${email} has been registered!`
-    })
-
 })
 
+app.post('/login', async (req, res) => {
 
+    const {email, password} = req.body;
+
+    try {
+        
+        // CHECK IF THERE IS ANY EMAIL REGISTERED
+        const checkEmail = await db.execute('SELECT email FROM student WHERE email = ?', [email]);
+        if (checkEmail.length <= 0){
+            return res.status(401).json({ error: 'Access Forbidden.'});
+        }
+        
+        // CHECK IF PASSWORD MATCH
+        const [emailPass] = await db.execute('SELECT password FROM student WHERE email = ?', [email]);
+        console.log(emailPass[0].password);
+        const passwordMatch = await bcrypt.compare(password, emailPass[0].password);
+
+        if(!passwordMatch){
+            return res.status(401).json({ error: 'Access Forbidden.' });
+        }
+
+        res.status(200);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error!' });
+    }
+
+})
 
 
 
