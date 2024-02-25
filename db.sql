6503bd3e718991f14e1d6e71af2da139b7a50e85
Zufar <69440085+Faralha@users.noreply.github.com>
2024-02-26 05:58:00 +0700

    JWT, admin login
    admin login doesn't require other registration form. But manual insertion is needed

--- db.sql
+++ db.sql
@@ -10,6 +10,13 @@ CREATE TABLE 'student' (
 	PRIMARY KEY ('id')
 );
 
+CREATE TABLE admin (
+	id VARCHAR(10) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
 INSERT INTO 'student' ('id','name','email', 'password', 'city', 'address', 'phonenumber')
 VALUES ('SJKT-0001', 'Muhammad Zufar Al Hafidz','faralhaonly@gmail.com', 'dawdawdas', 'Tangerang Selatan', 'Jl. Walet', '083021730130');
 
@@ -19,5 +26,7 @@ VALUES ('SJKT-0002', 'Muhammad Zufar Al Hafidz','faralhaonly@gmail.com', 'dawdaw
 INSERT INTO 'student' ('id','name','email', 'password', 'city', 'address', 'phonenumber')
 VALUES (?, ?, ?, ?, ?, ?, ?);
 
+INSERT INTO admin (id, name, email)
+VALUES ('SJKT-0004', 'Anjay', 'anjay@gmail.com');
 
 SELECT id FROM student ORDER BY id ASC LIMIT 1;
--- index.js
+++ index.js
@@ -12,15 +12,47 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(cors());
 
+// JWT VERIFICATION MIDDLEWARE
+const verifyToken = (token, callback) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, function(err, authData) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, authData);
+      }
+    });
+  };
+
+const authenticateToken = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+  
+    if (bearerHeader !== 'undefined') {
+      const bearerToken = bearerHeader.split(' ')[1];
+      verifyToken(bearerToken, (err, authData) => {
+        if (err) {
+          res.status(403).json({message: err}); // Unauthorized
+        } else {
+          req.authData = authData;
+          next(); // Lanjut ke handler berikutnya
+        }
+      });
+    } else {
+      res.status(403).json({message: "error"}); // Unauthorized
+    }
+  };
+  
+
 
 // USERS -- Dummies
-app.get('/user', (req, res) => {
+app.get('/user', authenticateToken, (req, res) => {
+
+    
     res.json({
         message: 'This is user page get request.'
     });
 })
 
-// REGISTER (kurang di email kalau udah pernah pakai email yang sama.)
+// REGISTER (kurang penggunaan akronim kota - atau gausah pakai aja, ya?)
 app.post('/register', async (req, res) => {
 
     var { email, name, password, city, address, phonenumber } = req.body;
@@ -51,7 +83,7 @@ app.post('/register', async (req, res) => {
         let newId = role + city + '-' + lastId.toString().padStart(4, '0');
 
         await db.execute(`INSERT INTO student ( id, name, email, password, city, address, phonenumber) VALUES (?, ?, ?, ?, ?, ?, ?);`,
-        [newId, name, email, hashedPassword, city, address, phonenumber]);
+        [newId, name, email, hashedPassword, city || null, address || null, phonenumber]);
 
         res.status(200).json({ message: 'Akun Berhasil Dibuat!'});
 
@@ -64,6 +96,7 @@ app.post('/register', async (req, res) => {
 
 })
 
+// LOGIN
 app.post('/login', async (req, res) => {
 
     const {email, password} = req.body;
@@ -85,7 +118,11 @@ app.post('/login', async (req, res) => {
             return res.status(401).json({ error: 'Access Forbidden.' });
         }
 
-        res.status(200);
+        // ROLE - harus dikonfig lagi sih wkwk
+        const role = "student"
+
+        const token = generateAccessToken({email, role});
+        res.json({token: token});
 
     } catch (error) {
         console.error(error);
@@ -93,6 +130,39 @@ app.post('/login', async (req, res) => {
     }
 
 })
+
+app.post('/admin/login', authenticateToken, async (req, res) => {
+
+  const token = req.headers['authorization'].split(' ')[1];
+
+  try {
+
+    const email = jwt.verify(token, process.env.TOKEN_SECRET).email;
+
+    const [verify, error] = await db.execute('SELECT email FROM admin WHERE email = ?', [email]);
+
+    if (email !== verify[0].email || error){
+      res.status(400).json({error: 'Unauthorized'})
+    }
+
+    res.status(200).json({message: "Authorized"});
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Internal Server Error!'});
+  }
+
+})
+
+app.post('/getUsers', authenticateToken, async (req, res) => {
+
+})
+
+function generateAccessToken(email){
+    return jwt.sign((email), process.env.TOKEN_SECRET, {
+      expiresIn: '1d'
+    });
+}
 
 
 
